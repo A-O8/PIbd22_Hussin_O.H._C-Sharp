46bededabe0b3137c19f5bf53b75a4d02471ee63
DESKTOP-KQPF5K4 <omerhanesh40@gmail.com>
2020-12-21 16:58:03 +0400

    lab1 is ready

---
+++ WindowsFormsAppAircraftcarrier/WindowsFormsAppAircraftcarrier/WindowsFormsAppAircraftcarrier/FormAircraft carrier.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppAircraftcarrier
+{
+    public partial class FormAircraft_carrier : Form
+    {
+
+        private Aircraft_carrier ship;
+        public FormAircraft_carrier()
+        {
+            InitializeComponent();
+        }
+        private void Draw()
+        {
+            Bitmap bmp = new Bitmap(pictureBoxAircraft_carrier.Width, pictureBoxAircraft_carrier.Height);
+            Graphics gr = Graphics.FromImage(bmp);
+            ship.DrawCar(gr);
+            pictureBoxAircraft_carrier.Image = bmp;
+        }
+
+        private void Aircraftcarrier_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            ship = new Aircraft_carrier(rnd.Next(100, 300), rnd.Next(100, 2000), Color.Blue,
+           Color.Yellow, true, true, true);
+            ship.setposition(rnd.Next(10, 100), rnd.Next(10, 100), pictureBoxAircraft_carrier.Width,
+          pictureBoxAircraft_carrier.Height);
+            Draw();
+
+        }
+
+        private void MoveAircraftcarrier_Click(object sender, EventArgs e)
+        {
+            string name = (sender as Button).Name;
+            switch (name)
+            {
+                case "buttonUp":
+                    ship.MoveTransport(Direction.Up);
+                    break;
+                case "buttonDown":
+                    ship.MoveTransport(Direction.Down);
+                    break;
+                case "buttonLeft":
+                    ship.MoveTransport(Direction.Left);
+                    break;
+                case "buttonRight":
+                    ship.MoveTransport(Direction.Right);
+                    break;
+            }
+            Draw();
+        }
+
+       
+    }
+}
