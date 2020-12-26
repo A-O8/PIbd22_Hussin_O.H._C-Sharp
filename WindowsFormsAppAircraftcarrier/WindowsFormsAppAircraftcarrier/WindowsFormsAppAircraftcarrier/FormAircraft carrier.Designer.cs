799d652c866b58a3ed7453baf162da0dff44fc81
DESKTOP-KQPF5K4 <omerhanesh40@gmail.com>
2020-12-26 04:21:06 +0400

    lab1 is ready

---
+++ WindowsFormsAppAircraftcarrier/WindowsFormsAppAircraftcarrier/WindowsFormsAppAircraftcarrier/FormAircraft carrier.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace WindowsFormsAppAircraftcarrier
+{
+    partial class FormAircraft_carrier
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttonRight = new System.Windows.Forms.Button();
+            this.buttonLeft = new System.Windows.Forms.Button();
+            this.buttonUp = new System.Windows.Forms.Button();
+            this.buttonDown = new System.Windows.Forms.Button();
+            this.pictureBoxAircraft_carrier = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxAircraft_carrier)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 46);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "СОЗАТЬ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Aircraftcarrier_Click);
+            // 
+            // buttonRight
+            // 
+            this.buttonRight.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonRight.BackgroundImage = global::WindowsFormsAppAircraftcarrier.Properties.Resources.Скриншот_02_08_2020_094237;
+            this.buttonRight.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonRight.Location = new System.Drawing.Point(750, 404);
+            this.buttonRight.Name = "buttonRight";
+            this.buttonRight.Size = new System.Drawing.Size(50, 46);
+            this.buttonRight.TabIndex = 1;
+            this.buttonRight.UseVisualStyleBackColor = true;
+            this.buttonRight.Click += new System.EventHandler(this.MoveAircraftcarrier_Click);
+            // 
+            // buttonLeft
+            // 
+            this.buttonLeft.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonLeft.BackgroundImage = global::WindowsFormsAppAircraftcarrier.Properties.Resources.Скриншот_02_08_2020_094108;
+            this.buttonLeft.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonLeft.Location = new System.Drawing.Point(631, 404);
+            this.buttonLeft.Name = "buttonLeft";
+            this.buttonLeft.Size = new System.Drawing.Size(56, 40);
+            this.buttonLeft.TabIndex = 1;
+            this.buttonLeft.UseVisualStyleBackColor = true;
+            this.buttonLeft.Click += new System.EventHandler(this.MoveAircraftcarrier_Click);
+            // 
+            // buttonUp
+            // 
+            this.buttonUp.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonUp.BackgroundImage = global::WindowsFormsAppAircraftcarrier.Properties.Resources.Скриншот_02_08_2020_094316;
+            this.buttonUp.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonUp.Location = new System.Drawing.Point(693, 352);
+            this.buttonUp.Name = "buttonUp";
+            this.buttonUp.Size = new System.Drawing.Size(51, 46);
+            this.buttonUp.TabIndex = 1;
+            this.buttonUp.UseVisualStyleBackColor = true;
+            this.buttonUp.Click += new System.EventHandler(this.MoveAircraftcarrier_Click);
+            // 
+            // buttonDown
+            // 
+            this.buttonDown.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonDown.BackgroundImage = global::WindowsFormsAppAircraftcarrier.Properties.Resources.Скриншот_02_08_2020_093703;
+            this.buttonDown.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonDown.Location = new System.Drawing.Point(693, 404);
+            this.buttonDown.Name = "buttonDown";
+            this.buttonDown.Size = new System.Drawing.Size(51, 46);
+            this.buttonDown.TabIndex = 1;
+            this.buttonDown.UseVisualStyleBackColor = true;
+            this.buttonDown.Click += new System.EventHandler(this.MoveAircraftcarrier_Click);
+            // 
+            // pictureBoxAircraft_carrier
+            // 
+            this.pictureBoxAircraft_carrier.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxAircraft_carrier.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxAircraft_carrier.Name = "pictureBoxAircraft_carrier";
+            this.pictureBoxAircraft_carrier.Size = new System.Drawing.Size(800, 450);
+            this.pictureBoxAircraft_carrier.TabIndex = 0;
+            this.pictureBoxAircraft_carrier.TabStop = false;
+            // 
+            // FormAircraft_carrier
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonRight);
+            this.Controls.Add(this.buttonLeft);
+            this.Controls.Add(this.buttonUp);
+            this.Controls.Add(this.buttonDown);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBoxAircraft_carrier);
+            this.Name = "FormAircraft_carrier";
+            this.Text = "FormAircraft_carrier";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxAircraft_carrier)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxAircraft_carrier;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonDown;
+        private System.Windows.Forms.Button buttonUp;
+        private System.Windows.Forms.Button buttonLeft;
+        private System.Windows.Forms.Button buttonRight;
+    }
+}
